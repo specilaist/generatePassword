ea4981088c9e20577d87e4104aff29468f9635ad
specilaist <specilaist@msn.com>
2020-07-28 15:00:35 -0700

    made passwordLength conditions, added confirms, started another if/else

--- Develop/script.js
+++ Develop/script.js
@@ -12,6 +12,21 @@ function writePassword() {
 
 function generatePassword() {
   var passwordLength = prompt('Please specify a password length more than 8 and less than 128')
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert('you need to have a number > 8 and < 128');
+    return;
+  }
+
+  var lowercase = confirm('Do you want lowercase?')
+  var uppercase = confirm('Do you want uppercase?')
+  var symbol = confirm('Do you want a symbol')
+  var number = confirm('Do you want a number?')
+
+  if (lowercase === true || uppercase === true || symbol === true || number === true) {
+    
+  }
+
+
   
 }
 
